9c622f9acde46e6322cbdbca7b0b86c64c303e17
Alexey Kuleshov <kulesa@gmail.com>
2011-10-26 13:12:50 +0400

    A passing spec added

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "http://rubygems.org"
+source "http://gems.github.com"
+
+group :test do
+  gem 'rake'
+  gem 'rspec'
+  gem 'sqlite3'
+end
---
+++ spec/sanity_spec.rb
@@ -0,0 +1,7 @@
+require File.expand_path('../spec_helper', __FILE__)
+
+describe 'It works' do
+  it "should be alive, alive" do
+    Class.class.should eq(Class)
+  end
+end
